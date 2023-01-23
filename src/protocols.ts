24c21390c63be7b1476452a26232f969d943e8e4
lodustitan <felipealbertooliveira@gmail.com>
2023-01-23 14:59:25 -0300

    1º

---
+++ src/protocols.ts
@@ -0,0 +1,28 @@
+export type ApplicationError = {
+  name: string;
+  message: string;
+};
+
+export type ViaCEPAddress = {
+  logradouro: string,
+  complemento: string,
+  bairro: string,
+  localidade: string,
+  uf: string,
+};
+
+export type ViaCEPAddressEnrollments = {
+  logradouro: string,
+  complemento: string,
+  bairro: string,
+  cidade: string,
+  uf: string,
+};
+
+export type RequestError = {
+  status: number,
+  data: object | null,
+  statusText: string,
+  name: string,
+  message: string,
+};
